a01a1ac5743bc81b56c2e9c78b76dac54a6cd39c
Mastie1 <mkm3361@gmail.com>
2025-07-18 11:17:29 +0300

    Final commit, full project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
+import { useReducer } from "react";
+import TicketForm from "./components/TicketForm";
+import ticketReducer from "./reducers/ticketReducer";
+import TicketItem from "./components/TicketItem";
+import TicketList from "./components/TicketList";
+import { sortTickets } from "./utilities/sortingUtilities";
 
 function App() {
+  const initialState = {
+    tickets: [],
+    editingTicket: null,
+    sortPreference: "High to low",
+  };
+
+  const [state, dispatch] = useReducer(ticketReducer, initialState);
+  const sortedTickets = sortTickets(state.tickets, state.sortPreference);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1> Todos List</h1>
+        <TicketForm dispatch={dispatch} editingTicket={state.editingTicket} />
+
+        {state.tickets.length > 0 && (
+          <div className="results">
+            <h2>All Tickets</h2>
+
+            <select
+            value={state.sortPreference}
+              onChange={(e) =>
+                dispatch({ type: "SET_SORTING", payload: e.target.value })
+              }
+            >
+              <option value={"High to low"}>High to low</option>
+              <option value={"Low to high"}>Low to high</option>
+            </select>
+
+            <TicketList
+              tickets={sortedTickets}
+              dispatch={dispatch}
+            ></TicketList>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
